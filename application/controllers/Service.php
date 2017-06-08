49af10f790367b73e45ec66dafc279322cfd150f
ramkumarizaap <ramkumar.izaap@gmail.com>
2017-06-08 16:01:18 +0530

    Punitha: Created webservices for credit card section

--- application/controllers/Service.php
+++ application/controllers/Service.php
@@ -19,7 +19,7 @@ class Service extends REST_Controller
             // Construct our parent class
             parent::__construct();
 
-            $this->load->model(array("user_model","login_model","address_model"));
+            $this->load->model(array("user_model","login_model","address_model","credit_card_model"));
             $key  = $this->get('X-APP-KEY');
         }
         
@@ -77,7 +77,7 @@ class Service extends REST_Controller
         //user email update
         function user_email_update_post() {
             
-            if(!$this->post('user_id') && !$this->post('user_id')){
+            if(!$this->post('user_id') && !$this->post('email')){
     			return $this->response(array('status' => 'error','msg' => 'Required fields missing in your request','error_code' => 1), 404);
     		}
              
@@ -223,6 +223,174 @@ class Service extends REST_Controller
             else
             {
                 return $this->response(array('status' =>'error','request_type' => 'get_username', 'msg' => "User doesn't exists!" ,'error_code' => 6), 404);
+            }
+        }
+        
+        //add credit card
+        function add_credit_card_post()
+        {
+            if(!$this->post('user_id')){
+    			return $this->response(array('status' => 'error','msg' => 'Required fields missing in your request','error_code' => 1), 404);
+    		} 
+            
+            $name           = $this->post('name');
+            $card_number    = $this->post('card_number');
+            $card_type      = $this->post('card_type');
+            $exp_month      = $this->post('exp_month');
+            $exp_year       = $this->post('exp_year');
+            $cvv            = $this->post('cvv');
+            $employer_id    = $this->post('employer_id');
+            $address_card   = $this->post('address_card');
+            $state          = $this->post('state');
+            $city           = $this->post('city');
+            $zipcode        = $this->post('zipcode');
+            $default_card   = $this->post('default_card');
+            
+             if(!empty($card_number)) {
+                  //Card number check if already exists or not
+                  $card_res = $this->credit_card_model->check_unique(array("card_number" => $card_number));
+                  if(count($card_res)>0) {
+                    return $this->response(array('status' => "error",'msg' => 'Card number already exists.','error_code' => 2), 404);
+                  }
+             }
+            
+            $ins_data = array();
+            $ins_data['name']        = $name;
+            $ins_data['card_number'] = $card_number;
+            $ins_data['card_type']   = $card_type;
+            $ins_data['exp_month']   = $exp_month;
+            $ins_data['exp_year']    = $exp_year;
+            $ins_data['cvv']         = $cvv;
+            $ins_data['address_card']= $address_card;
+            $ins_data['state']       = $state;
+            $ins_data['city']        = $city;
+            $ins_data['zipcode']     = $zipcode;
+            $ins_data['employer_id'] = $employer_id;
+            $ins_data['default_card']= $default_card;
+            
+            $card_id                 = $this->credit_card_model->insert($ins_data);
+            if(!empty($card_id)){
+                return $this->response(array('status' =>'success','request_type' => 'add_credit_card','card_id' => $card_id), 200);
+            }    
+            else
+            {
+                return $this->response(array('status' =>'error','request_type' => 'add_credit_card', 'msg' => "Card details doesn't create!" ,'error_code' => 5), 404);
+            }
+        }
+        
+        //user email update
+        function credit_card_update_post() {
+            
+            if(!$this->post('card_id') && !$this->post('card_number')){
+    			return $this->response(array('status' => 'error','msg' => 'Required fields missing in your request','error_code' => 1), 404);
+    		}
+             
+                $name           = $this->post('name');
+                $card_number    = $this->post('card_number');
+                $card_type      = $this->post('card_type');
+                $exp_month      = $this->post('exp_month');
+                $exp_year       = $this->post('exp_year');
+                $cvv            = $this->post('cvv');
+                $employer_id    = $this->post('employer_id');
+                $address_card   = $this->post('address_card');
+                $state          = $this->post('state');
+                $city           = $this->post('city');
+                $zipcode        = $this->post('zipcode');
+                $card_id        = $this->post('card_id');
+                $default_card   = $this->post('default_card');
+                
+                //get card details
+                $cd_res = $this->credit_card_model->check_unique(array('id' => $card_id));
+                
+                $ins_data = array();
+                $ins_data['name']        = (!empty($name))?$name:$cd_res['name'];
+                $ins_data['card_number'] = (!empty($card_number))?$card_number:$cd_res['card_number'];
+                $ins_data['card_type']   = (!empty($card_type))?$card_type:$cd_res['card_type'];
+                $ins_data['exp_month']   = (!empty($exp_month))?$exp_month:$cd_res['exp_month'];
+                $ins_data['exp_year']    = (!empty($exp_year))?$exp_year:$cd_res['exp_year'];
+                $ins_data['cvv']         = (!empty($cvv))?$cvv:$cd_res['cvv'];
+                $ins_data['address_card']= (!empty($address_card))?$address_card:$cd_res['address_card'];
+                $ins_data['state']       = (!empty($state))?$state:$cd_res['state'];
+                $ins_data['city']        = (!empty($city))?$city:$cd_res['city'];
+                $ins_data['zipcode']     = (!empty($zipcode))?$zipcode:$cd_res['zipcode'];
+                $ins_data['employer_id'] = (!empty($employer_id))?$employer_id:$cd_res['employer_id'];
+                $ins_data['default_card']= (!empty($default_card))?$default_card:$cd_res['default_card'];
+           
+             if(!empty($card_number)) {
+                  //Card number check if already exists or not
+                  $card_res = $this->credit_card_model->check_unique(array("card_number" => $card_number, 'id!=' => $card_id));
+                  if(count($card_res)>0) {
+                    return $this->response(array('status' => "error",'msg' => 'Card number already exist.','error_code' => 2), 404);
+                  }
+             } 
+            
+            $ins_data                 = array();
+            $ins_data['card_number']  = $card_number;  
+            $affected                 = $this->credit_card_model->update(array("id" => $card_id),$ins_data);
+            
+            if(!empty($affected)){   
+                $res = $this->credit_card_model->check_unique(array('id' => $card_id));
+                return $this->response(array('status' =>'success','request_type' => 'update_credit_card_details','user_data' => $res), 200);
+            }
+            else
+            {
+                return $this->response(array('status' =>'error','request_type' => 'update_credit_card_details', 'msg' => "Credit Cart doesn't updated", 'error_code' => 3), 404);
+            } 
+        }
+        
+         //view credit card details
+        function view_credit_card_post()
+        {
+            if(!$this->post('card_id')){
+    			return $this->response(array('status' => 'error','msg' => 'Required fields missing in your request','error_code' => 1), 404);
+    		}
+            
+            $card_id   = $this->post('card_id');
+            
+            //Card number check if already exists or not
+            $card_res  = $this->credit_card_model->check_unique(array('id' => $card_id));
+            
+            if(count($card_res)>0){
+                 return $this->response(array('status' =>'success','request_type' => 'view_credit_card_details', 'card_data' => $card_res), 200);
+            }
+            else
+            {
+                return $this->response(array('status' =>'error','request_type' => 'view_credit_card_details', 'msg' => "Card doesn't exists!" ,'error_code' => 6), 404);
+            }
+        } 
+        
+       
+       //Delete credit card details
+        function delete_credit_card_post()
+        {
+            if(!$this->post('card_id')){
+    			return $this->response(array('status' => 'error','msg' => 'Required fields missing in your request','error_code' => 1), 404);
+    		}
+            
+            $card_id   = $this->post('card_id');
+            $res       = $this->credit_card_model->delete(array('id' => $card_id));
+            
+            return $this->response(array('status' =>'success','request_type' => 'delete_credit_card', 'card_id' => $card_id), 200);
+        } 
+        
+        //lists the employer cards
+        function lists_employer_cards_post()
+        {
+            if(!$this->post('employer_id')){
+    			return $this->response(array('status' => 'error','msg' => 'Required fields missing in your request','error_code' => 1), 404);
+    		}
+            
+            $employer_id= $this->post('employer_id');
+            
+            $card_res   = array();
+            $card_res   = $this->credit_card_model->get_employer_cards(array('c.employer_id' => $employer_id));
+            
+            if(count($card_res)>0){
+                 return $this->response(array('status' =>'success','request_type' => 'employer_card_lists', 'card_data' => $card_res), 200);
+            }
+            else
+            {
+                return $this->response(array('status' =>'error','request_type' => 'employer_card_lists', 'msg' => "No Cards Found!" ,'error_code' => 7), 404);
             }
         }
 }
---
+++ application/models/Credit_card_model.php
@@ -0,0 +1,45 @@
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Credit_card_model extends CI_model {
+    
+    function __construct()
+    {
+        parent::__construct();
+        $this->_table = 'credit_card';
+    }
+   
+    public function insert($data)
+	{
+		$q =  $this->db->insert($this->_table ,$data);
+		return $this->db->insert_id();
+	}
+   	
+    public function update($where,$data)
+   {
+    $this->db->where($where);
+    $this->db->update($this->_table ,$data);
+   }
+
+  public function delete($where)
+  {
+    $this->db->where($where);
+    $this->db->delete($this->_table );
+  }
+  
+    function check_unique($where)
+    {
+        $this->db->select("*");
+        $this->db->where($where);
+        return $this->db->get($this->_table)->row_array();
+    }
+    
+    function get_employer_cards($where)
+    {
+        $this->db->select("c.*,u.firstname,u.lastname");
+        $this->db->join("user u","u.id=c.employer_id");
+        $this->db->where($where);
+        return $this->db->get()->result_array();
+        
+    }
+}
+?>
